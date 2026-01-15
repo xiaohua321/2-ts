683b5c56931de779d8225d77aa1a060654e3dafa
xiaozhao <xiaozhao@tsign.cn>
2026-01-15 17:40:26 +0800

    ceshi

---
+++ src/utils/index.js
@@ -0,0 +1,19 @@
+/**
+ * 计算两个参数的和
+ * @param {number} a - 第一个参数
+ * @param {number} b - 第二个参数
+ * @returns {number} 两个参数的和
+ */
+export function add (a, b) {
+  return a + b
+}
+
+/**
+ * 计算两个参数的差
+ * @param {number} a - 被减数
+ * @param {number} b - 减数
+ * @returns {number} 两个参数的差
+ */
+export function subtract (a, b) {
+  return a - b
+}
